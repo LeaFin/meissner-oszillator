8ed1c6ca12f9db4155179858b811a82f9ccd7f62
Michael Brodmann <mib@doodle.com>
2014-04-05 19:19:39 +0200

    fixed euler

--- euler.py
+++ euler.py
@@ -1,4 +1,6 @@
-def euler(t_end, t_start, y_start, h, w):
+import numpy as np
+import math
+def euler(t_end, t_start, y_start, h):
     y = y_start
     t = t_start
     n = int((t_end - t_start) / h)
@@ -15,12 +17,12 @@ def w(t,z):
     C = 0.00001
     L = 0.2
     R = 30
-    u = U
+    u = U(t)
     Q = z[0]
     I = z[1]
-    Q2 = U/L - Q/(C*L) - (R*I)/L
-    res[0] = I
-    res[1] = Q2
+    Q2 = u/L - Q/(C*L) - (R*I)/L
+    res.append(I)
+    res.append(Q2)
     return res
 
 
