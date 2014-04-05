4bd07474f98a5b839998190e17013bcedfd48d0b
Michael Brodmann <mib@doodle.com>
2014-04-05 19:02:24 +0200

    euler

--- euler.py
+++ euler.py
@@ -8,3 +8,24 @@ def euler(t_end, t_start, y_start, h, w):
         y = y + k * h
         t = t + h
     return y
+
+
+def w(t,z):
+    res = []
+    C = 0.00001
+    L = 0.2
+    R = 30
+    u = U
+    Q = z[0]
+    I = z[1]
+    Q2 = U/L - Q/(C*L) - (R*I)/L
+    res[0] = I
+    res[1] = Q2
+    return res
+
+
+def U(t):
+    u = 10
+    a = 0
+    f = 400
+    return u + a * math.sin(2*3.14*f*t) 
