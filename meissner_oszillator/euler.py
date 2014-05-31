e0aac970ee59b4ba195136d5a365216c18f9a4e1
leandrafinger <leandra.finger@gmail.com>
2014-05-31 10:30:48 +0200

    comments added

--- meissner_oszillator/euler.py
+++ meissner_oszillator/euler.py
@@ -1,14 +1,16 @@
 from differential import w
 
 
+# Implementation of euler algorightm
 def euler(t_end, t_start, y_start, h, figure):
     y = y_start
     t = t_start
-    n = int((t_end - t_start) / h)
-    h = (t_end - t_start) / n
+    n = int((t_end - t_start) / h)  # number of steps in given time
+    h = (t_end - t_start) / n  # duration of timestep
 
     for i in range(n):
-        k = w(t, y)
+        # calling differatial equation for time and inputarray y, and inputvalues
+        k = w(t, y, r=figure.r, c=figure.c, u0=figure.u0, l1=figure.l1, l12=figure.l12)
         y = y + k * h
         t = t + h
-        figure.plot([t, y.item(0)])
+        figure.plot([t, y.item(0), y.item(1)]) # plot new point
